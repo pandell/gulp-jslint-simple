e04743d9f0b16dd5170f2399895b5a309864ecde
Milan Gardian <mgardian@gmail.com>
2014-06-16 08:06:30 -0600

    Use JSHint in addition to JSLint during development

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 "use strict";
 
 var gulp = require("gulp");
+var jshint = require("gulp-jshint");
 var mocha = require("gulp-mocha");
 var monitorCtrlC = require("monitorctrlc");
 var taskFromStreams = require("gulp-taskfromstreams");
@@ -11,11 +12,13 @@ var jslint = require("./");
 
 var rootFiles = "*.js*";
 var libFiles = "lib/*.js";
+var libExcludes = "!lib/jslint.js";
 var testFiles = "test/*.js";
 
 gulp.task("lint", taskFromStreams(function () {
     return [
-        gulp.src([rootFiles, libFiles, testFiles]),
+        gulp.src([rootFiles, libFiles, libExcludes, testFiles]),
+        jshint(),
         jslint.run(),
         jslint.report({ emitErrorAtEnd: true })
     ];
@@ -30,6 +33,6 @@ gulp.task("test", ["lint"], taskFromStreams(function () {
 
 gulp.task("watch", function () {
     monitorCtrlC();
-    gulp.watch([rootFiles, libFiles, testFiles], ["test"]);
+    gulp.watch([rootFiles, libFiles, libExcludes, testFiles], ["test"]);
     gulp.start("test");
 });
